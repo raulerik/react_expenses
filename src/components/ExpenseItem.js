019279850de71e0eccd78858e6c721a7a18457cd
Raul-Erik Laur <raul.erik.laur@gmail.com>
2024-01-30 13:47:38 +0200

    Ylesanne1 - Kujunduse lisamine komponendile

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
-    const month = props.expenseData.date.toLocaleString('en-US', {month: 'long'})
-    const day = props.expenseData.date.toLocaleString('en-US', {day: '2-digit'})
-    const year = props.expenseData.date.getFullYear()
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}€</div>
@@ -20,4 +14,4 @@ function ExpenseItem(props){
     )
 }
 
-export default ExpenseItem
+export default ExpenseItem;
